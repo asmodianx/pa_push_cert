ccb066a634e766a093a9d6dc4782ce625db3cd48
Asmodian X <asmodianx@users.noreply.github.com>
2019-05-13 14:18:40 -0500

    Update pa_push_cert.py

--- pa_push_cert.py
+++ pa_push_cert.py
@@ -37,8 +37,8 @@ commit_api_uri="https://" + pa_host + "/api/?type=commit&key=" + api_key + "&cmd
 response = requests.get(commit_api_uri ,verify=False)
 #todo: read http code and return an exit code back to the os to make it more script friendly.
 if response.status_code == 200:
-    print('PA Commit Success')
+    print('PA Commit: Success')
 elif response.status_code == 404:
-    print('PA Commit Failed')
+    print('PA Commit: Failed')
 
 
