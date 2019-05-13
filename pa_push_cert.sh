be72bb58e51a3c19386dd8404b7ea98b93191bea
Asmodian X <asmodianx@users.noreply.github.com>
2019-05-13 13:23:11 -0500

    Create pa_push_cert.sh

---
+++ pa_push_cert.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+script_path="/usr/local/sbin"
+if [ -d /etc/letsencrypt/live ]; then
+        cd /etc/letsencrypt/live
+		for file in /etc/letsencrypt/live/*; do
+				cd $file
+				fn=`echo $file|cut -d \/ -f5`
+				name=`hostname`
+				python $script_path/pa_push_cert.py $name
+				echo uploaded : $name
+				rm le_export.pkcs12
+		done
+fi
+else
+    echo "Error: Lets Encrypt folder not found"
+fi
